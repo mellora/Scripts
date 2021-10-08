686f4e67fe8ca22ae303d1c1abfd523afb2e680f
Alexander Mellor <AlexMellor2014@gmail.com>
2021-10-08 16:18:23 -0500

    MODIFIED: made to reflect the effects of the test_tmux.py script.

--- tmux_test.sh
+++ tmux_test.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-# Session Name
-session="Tester"
-window="MAIN"
+# Script vars
+SESSION_NAME="TEST"
+WINDOW_1_NAME="test_bash"
+WINDOW_1_COMMAND="ls"
+WINDOW_2_NAME="HTOP"
+WINDOW_2_COMMAND="htop"
 
-# Start new Session with our name
-tmux new-session -d -s $session
+SESSION_EXISTS=$(tmux list-sessions | grep $SESSION_NAME)
 
-# Name first Window and start htop
-tmux rename-window -t 0 $window
-tmux send-keys -t $window 'htop' C-m
+if [ "$SESSION_EXISTS" = "" ]
+then
+    # Start new Session with our name
+    tmux new-session -d -s $SESSION_NAME
+
+    # Name first Window and start run command
+    tmux rename-window -t 0 $WINDOW_1_NAME
+    tmux send-keys -t $WINDOW_1_NAME $WINDOW_1_COMMAND C-m
+
+    # Create second window and run htop command
+    tmux new-window -t $session_name:1 -n $WINDOW_2_NAME
+    tmux send-keys -t $WINDOW_2_NAME $WINDOW_2_COMMAND C-m
+fi
