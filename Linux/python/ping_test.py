d2d0fa0c86c99c32a42b41609751a6a1695379e6
Alexander Mellor <AlexMellor2014@gmail.com>
2021-12-13 11:09:32 -0600

    MODIFIED: moved files into a new folder structure for split between Linux and Windows Scripts

---
+++ Linux/python/ping_test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from subprocess import call
+
+ping_host = '8.8.8.8'
+interfaces = {
+    'wireless': 'wlp6s0',
+    'wired': 'enp7s0'
+}
+number_of_pings = 4
+
+interface = interfaces['wireless']
+
+if __name__ == '__main__':
+    print('')
+    print('**************************************************')
+    print(f'***  From {interface}:')
+    print('**************************************************')
+    call([
+        'ping',
+        '-c',
+        str(number_of_pings),
+        '-I',
+        interface,
+        ping_host
+    ])
+    print('')
